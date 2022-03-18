d4afe5b3a21dd9bd290da315b03314d2c2558888
DanielPickens <72703981+DanielPickens@users.noreply.github.com>
2022-03-17 18:12:46 -0600

    commit on main

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,20 @@ require 'minitest/autorun'
 require 'webmock/minitest'
 require 'json'
 require 'prophecy'
+require 'opentelemetry/sdk'
 
 
 def open_test_file(name)
   File.new(File.join(File.dirname(__FILE__), name.split('.').last, name))
+end
+
+
+
+
+# global opentelemetry-sdk setup:
+EXPORTER = OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new
+span_processor = OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(EXPORTER)
+
+OpenTelemetry::SDK.configure do |c|
+  c.add_span_processor span_processor
 end
